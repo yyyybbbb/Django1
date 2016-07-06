c0e9feb0673db93d97afa3699a0da0fe8c4787d8
yyyybbbb <1559068537@qq.com>
2016-07-06 09:14:26 -0700

    models.py

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	STATUS_CHOICE = (("draft","DRAFT"),("published","PUBLISHED"),)
+ 	
+
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique_for_date="published")
+	author = models.Foreignkey(User, related_name="blog_posts")
+	body = models.TextField()
+	published = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models,DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10,choices=STATUS_CHOICE,default="draft")
+	 class Meta:
+		ordering = ("-published",)
+	 def __str__(self):
+		return self.title
+
+
